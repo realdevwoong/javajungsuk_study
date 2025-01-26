7795fb91ccf58a0145b5392e019daaa12ec46bf8
realdevwoong <projung134@gmail.com>
2025-01-26 19:54:39 +0900

    Feat: "특수문자다루기"

---
+++ src/main/java/org/zerock/springex/javajungsuk/ch02/SpecialCharEx.java
@@ -0,0 +1,13 @@
+package org.zerock.springex.javajungsuk.ch02;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class SpecialCharEx {
+    public static void main(String[] args) {
+        System.out.println('\'');
+        System.out.println("abc\t123\tb456");
+        System.out.println('\n');
+        System.out.println("\"Hello\"");
+        System.out.println("c:\\");
+    }
+}
