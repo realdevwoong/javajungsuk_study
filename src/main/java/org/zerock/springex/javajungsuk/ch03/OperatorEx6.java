e6ba11b418dc1c9a261ae7022c755f32e6fafc01
realdevwoong <projung134@gmail.com>
2025-01-28 10:44:44 +0900

    byte+byte=int

---
+++ src/main/java/org/zerock/springex/javajungsuk/ch03/OperatorEx6.java
@@ -0,0 +1,11 @@
+package org.zerock.springex.javajungsuk.ch03;
+
+public class OperatorEx6 {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+        int c = a + b; //c는 int로 변환되므로 int라고 명시적
+        System.out.println(c);
+
+    }
+}
