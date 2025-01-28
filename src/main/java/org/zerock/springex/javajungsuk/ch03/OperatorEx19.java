d0e99049dd50f3506136058b518b0235ce5d4c5e
realdevwoong <projung134@gmail.com>
2025-01-28 12:14:30 +0900

    divive %

--- src/main/java/org/zerock/springex/javajungsuk/ch03/OperatorEx19.java
+++ src/main/java/org/zerock/springex/javajungsuk/ch03/OperatorEx19.java
@@ -1,6 +1,7 @@
 package org.zerock.springex.javajungsuk.ch03;
 
-public class OperatorEx19 {
+public class
+OperatorEx19 {
     public static void main(String[] args) {
         int x = 10;
         int y = 8;
@@ -8,3 +9,5 @@ public class OperatorEx19 {
         System.out.printf("목은 %d이고, 나머지는 %d입니다.%n", x/y, x%y);
     }
 }
+//10을 8로 나누면,
+//목은 1이고, 나머지는 2입니다.
