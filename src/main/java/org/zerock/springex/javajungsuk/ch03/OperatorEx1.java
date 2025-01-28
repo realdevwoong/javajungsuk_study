3abe11b4e476aec55a75bc57728e8d809f5ca18e
realdevwoong <projung134@gmail.com>
2025-01-28 10:12:20 +0900

    prefix, postfix

---
+++ src/main/java/org/zerock/springex/javajungsuk/ch03/OperatorEx1.java
@@ -0,0 +1,12 @@
+package org.zerock.springex.javajungsuk.ch03;
+
+public class OperatorEx1 {
+    public static void main(String[] args) {
+        int  i = 5;
+        i++;
+        System.out.println(i);
+        i = 5;
+        ++i;
+        System.out.println(i);
+    }
+}
