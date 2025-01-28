50df7f44b3e951e544a770c77f34b14a9556b103
realdevwoong <projung134@gmail.com>
2025-01-28 12:15:20 +0900

    divde minus

---
+++ src/main/java/org/zerock/springex/javajungsuk/ch03/OperatorEx20.java
@@ -0,0 +1,10 @@
+package org.zerock.springex.javajungsuk.ch03;
+
+public class OperatorEx20 {
+    public static void main(String[] args) {
+        System.out.println("-10%8"); //-2
+        System.out.println("10%-8");//2
+        System.out.println("-10%-8");//-2
+
+    }
+}
