679e5c5190e6765b642b61b99adeb06eb870963d
realdevwoong <projung134@gmail.com>
2025-01-28 11:59:45 +0900

    round use Math()

---
+++ src/main/java/org/zerock/springex/javajungsuk/ch03/OperatorEx18.java
@@ -0,0 +1,9 @@
+package org.zerock.springex.javajungsuk.ch03;
+
+public class OperatorEx18 {
+    public static void main(String[] args) {
+        double pi = 3.141592;
+        double shortPi = Math.round(pi * 100) / 100.0;
+        System.out.println(shortPi);
+    }
+}
