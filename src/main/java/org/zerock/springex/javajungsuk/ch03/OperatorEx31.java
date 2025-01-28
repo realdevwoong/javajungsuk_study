382055836ce5ab1422094c65895d658d4ec3d55a
realdevwoong <projung134@gmail.com>
2025-01-28 18:08:22 +0900

    ãbitwise operator

---
+++ src/main/java/org/zerock/springex/javajungsuk/ch03/OperatorEx31.java
@@ -0,0 +1,28 @@
+package org.zerock.springex.javajungsuk.ch03;
+
+public class OperatorEx31 {
+    public static void main(String[] args) {
+        int dec  = 1234;
+        int hex  = 0xABCD;
+        int mask = 0xF;
+
+        System.out.printf("hex=%X%n", hex);
+        System.out.printf("%X%n", hex & mask);
+
+        hex = hex >> 4;
+        System.out.printf("%X%n", hex & mask);
+
+        hex = hex >> 4;
+        System.out.printf("%X%n", hex & mask);
+
+        hex = hex >> 4;
+        System.out.printf("%X%n", hex & mask);
+
+        //hex=ABCD
+        //D
+        //C
+        //B
+        //A
+    }
+
+}
