9ffb901609360f3044eddc4a1eb16d7689307835
realdevwoong <projung134@gmail.com>
2025-01-28 12:07:36 +0900

    % divide

---
+++ src/main/java/org/zerock/springex/javajungsuk/ch03/OperatorEx19.java
@@ -0,0 +1,10 @@
+package org.zerock.springex.javajungsuk.ch03;
+
+public class OperatorEx19 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 8;
+        System.out.printf("%d을 %d로 나누면, %n", x, y);
+        System.out.printf("목은 %d이고, 나머지는 %d입니다.%n", x/y, x%y);
+    }
+}
