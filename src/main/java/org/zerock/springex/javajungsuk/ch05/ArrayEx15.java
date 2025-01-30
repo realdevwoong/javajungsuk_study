35b72cf1c102faf90f13a5209388742a8b9588a2
realdevwoong <projung134@gmail.com>
2025-01-30 14:39:02 +0900

    array

---
+++ src/main/java/org/zerock/springex/javajungsuk/ch05/ArrayEx15.java
@@ -0,0 +1,19 @@
+package org.zerock.springex.javajungsuk.ch05;
+
+public class ArrayEx15 {
+    public static void main(String[] args) {
+        String source = "SOSHELP";
+        String[] morse = {".-", "-...", "-.-.","-..", "."
+                ,"..-.", "--.", "....","..",".---"
+                , "-.-", ".-..", "--", "-.", "---"
+                , ".--.", "--.-",".-.","...","-"
+                , "..-", "...-", ".--", "-..-","-.--"
+                , "--.." };
+        String result="";
+        for (int i=0; i < source.length() ; i++ ) {
+            result+=morse[source.charAt(i)-'A'];
+        }
+        System.out.println("source:" + source);
+        System.out.println("morse:" + result);
+    }
+}
